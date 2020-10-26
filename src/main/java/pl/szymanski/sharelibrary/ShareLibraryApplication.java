0499104b8dfe6827574e8bd95b724bc327a80179
Marek Szymański <szymanski_m@o2.pl>
2020-10-26 16:13:51 +0100

    Initial commit

---
+++ src/main/java/pl/szymanski/sharelibrary/ShareLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.szymanski.sharelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/szymanski/sharelibrary/ShareLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymanski.sharelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
