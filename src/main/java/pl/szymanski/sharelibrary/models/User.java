0fef25eeeb95534178b5854bcf2cdbd585b442d0
Marek Szymański <szymanski_m@o2.pl>
2020-10-31 14:41:26 +0100

    Added ManyToOne for Address.

--- src/main/java/pl/szymanski/sharelibrary/models/User.java
+++ src/main/java/pl/szymanski/sharelibrary/models/User.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import javax.persistence.*;
 import java.util.Set;
 
-import static javax.persistence.CascadeType.*;
+import static javax.persistence.CascadeType.MERGE;
+import static javax.persistence.CascadeType.PERSIST;
 
 @Entity
 @Data
@@ -15,19 +16,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String name;
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String surname;
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private char[] password;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String surname;
 
     @ManyToMany(cascade = {PERSIST, MERGE})
     @JoinTable(joinColumns = @JoinColumn(name = "userId"),
             inverseJoinColumns = @JoinColumn(name = "bookId"))
     private Set<Book> books;
 
+    @ManyToOne(cascade = {PERSIST, MERGE})
+    @JoinTable(name = "addressId")
+    private Address defaultAddress;
 }
