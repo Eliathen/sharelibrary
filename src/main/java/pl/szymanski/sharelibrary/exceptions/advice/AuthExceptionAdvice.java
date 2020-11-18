0b3a14c56336d5a9e004809ced2fde87beed7e7e
Marek Szymański <szymanski_m@o2.pl>
2020-11-18 20:20:52 +0100

    Added Handler for BadCredentialsException.class

--- src/main/java/pl/szymanski/sharelibrary/exceptions/advice/AuthExceptionAdvice.java
+++ src/main/java/pl/szymanski/sharelibrary/exceptions/advice/AuthExceptionAdvice.java
@@ -2,8 +2,10 @@ package pl.szymanski.sharelibrary.exceptions.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.szymanski.sharelibrary.exceptions.ExceptionMessages;
 import pl.szymanski.sharelibrary.exceptions.auth.InvalidUsernameEmailOrPassword;
 
 import java.time.LocalDateTime;
@@ -20,5 +22,10 @@ public class AuthExceptionAdvice {
         );
     }
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorInfo> badCredentialsExceptionHandler(BadCredentialsException ex) {
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), ExceptionMessages.INVALID_USERNAME_EMAIL_OR_PASSWORD);
+        return new ResponseEntity<>(errorInfo, HttpStatus.UNAUTHORIZED);
+    }
 
 }
