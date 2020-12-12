4290ef2eac27b04813a0d5bbf91c976811604b3d
Marek Szymański <szymanski_m@o2.pl>
2020-12-12 16:02:02 +0100

    Added new inserts to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ INSERT INTO public.user (id, email, name, password, surname, username, coordinat
     VALUES (1, 'poczta@pocztowska.pl', 'haslo jak', '$2a$10$NBwGzPSWNXVOlSBbVgdqyexaL4SvcSwXEu2ziXyQ4qooER.g5whgS', 'username', 'z', 1);
 INSERT INTO public.user (id, email, name, password, surname, username, coordinates_id)
     VALUES (2, 'poczta1@pocztowska.com', 'Wojciech', '$2a$10$aDoBYGbsQNHueWHeD92Jkeamw1rydYYXzeaufmu4hg8LyV00/h5E.', 'haslo x', 'x', 2);
+INSERT INTO PUBLIC.CHAT_ROOM (ID, RECIPIENT_ID, SENDER_ID) VALUES (1, 2, 1);
+INSERT INTO PUBLIC.CHAT_MESSAGE (ID, CONTENT, TIMESTAMP, ROOM_ID, RECIPIENT_ID, SENDER_ID) VALUES (1, 'Hello user 2', '2012-09-17 18:47:52.690000', 1, 2, 1);
+INSERT INTO PUBLIC.CHAT_MESSAGE (ID, CONTENT, TIMESTAMP, ROOM_ID, RECIPIENT_ID, SENDER_ID) VALUES (2, 'Hello user 1', '2012-09-17 18:47:52.690000', 1, 1, 2);
